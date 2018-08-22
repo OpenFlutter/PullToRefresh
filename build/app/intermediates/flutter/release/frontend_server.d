ca8c95c6c89b9b6a433c2a48c697b24bb425d4b3
dongbaolong <baoolong>
2018-08-22 14:42:02 +0800

    dragableGridView

--- build/app/intermediates/flutter/release/frontend_server.d
+++
@@ -1,1 +0,0 @@
-frontend_server.d: E:\Flutter\flutter\bin\cache\artifacts\engine\windows-x64\frontend_server.dart.snapshot
--- build/app/intermediates/flutter/release/snapshot.d
+++
@@ -1,1 +0,0 @@
-D:\\ProjectCode\\flutterapp\\build\\app\\intermediates\\flutter\\release\\vm_snapshot_data : 
